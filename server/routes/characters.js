392e05b4a400e53db56ac19fb64d9ca23d576643
vzbel <142253332+vzbel@users.noreply.github.com>
2025-10-17 16:27:30 -0600

    Define routes and update server

---
+++ server/routes/characters.js
@@ -0,0 +1,12 @@
+import express from "express";
+import CharactersController from "../controllers/characters.js";
+
+const router = express.Router();
+
+/* Routes to get, create, edit, and delete items */
+router.get("/", CharactersController.getCharacters); // all character data
+router.post("/", CharactersController.createCharacter); // new character
+router.put("/:id", CharactersController.updateCharacter); // edit existing character
+router.delete("/:id", CharactersController.deleteCharacter); // eliminate character
+
+export default router;
